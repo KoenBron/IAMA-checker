3f1a487bc71cb8e1a4db718a7affdd77fb3544e4
Koen Bron <koenbron2002@gmail.com>
2023-11-23 01:36:20 +0100

    Created assesment model and migrated the database

--- iama_checker/base/models.py
+++ iama_checker/base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Assesment(models.Model):
+    name = models.CharField(max_length=40)# Name of the assesment
+    organisation = models.CharField(max_length=50)# Name of the organisation performing the assesment
+    complete_status = models.BooleanField(default=False)
+    date_last_saved = models.DateField(auto_now=True)
