8777a52d008595046b3a51f6cee03a714e57445b
Koen Bron <koenbron2002@gmail.com>
2023-11-23 01:54:28 +0100

    Created a form to create or alter assesments

--- iama_checker/base/views.py
+++ iama_checker/base/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from forms import AssesmentForm
 
 # Create your views here.
 @login_required
 def greeting(request):
-    return render(request, "base/home_screen.html")
+    form = AssesmentForm()# To allow users to create a new assesment 
+    return render(request, "base/home_screen.html", {"form": form})
 
 def bink_test(request):
     return render(request, "base/temp_bink.html")
