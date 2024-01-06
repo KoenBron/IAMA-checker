1572a06ca67543d4183d6bf32b64189935eaaecd
Koen Bron <koenbron2002@gmail.com>
2024-01-06 22:24:04 +0100

    moderate changes to collaborator functionality

--- iama_checker/accounts/forms.py
+++ iama_checker/accounts/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username"]
+
+class UserLoginForm(AuthenticationForm):
+    # Init with the params of the class it inherits from
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(widget=(forms.TextInput(attrs={"class": "form-control"})))
+    password = forms.CharField(widget=(forms.PasswordInput(attrs={"class": "form-control"})))
