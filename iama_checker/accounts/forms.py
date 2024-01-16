a44d311b780857e16b9055b10e30b042739c7d06
Koen Bron <koenbron2002@gmail.com>
2024-01-16 15:51:40 +0100

    It's 1.30 am, fixed the stupid register bug which wasn't a bug at all but learned more about theform system of django so there's that

--- iama_checker/accounts/forms.py
+++ iama_checker/accounts/forms.py
@@ -2,15 +2,19 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
 
+# Simple forms to register and log in users
+
 class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ["username"]
+    # Alter widget settings by overriding them in the init function
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Give the proper class
+        self.fields["password1"].widget = forms.PasswordInput(attrs={"class": "form-control"})
+        self.fields["password2"].widget = forms.PasswordInput(attrs={"class": "form-control"})
+
+    username = UsernameField(widget=(forms.TextInput(attrs={"class": "form-control"})))
+
 
 class UserLoginForm(AuthenticationForm):
-    # Init with the params of the class it inherits from
-    def __init__(self, *args, **kwargs):
-        super(UserLoginForm, self).__init__(*args, **kwargs)
-
     username = UsernameField(widget=(forms.TextInput(attrs={"class": "form-control"})))
     password = forms.CharField(widget=(forms.PasswordInput(attrs={"class": "form-control"})))
--- iama_checker/accounts/views.py
+++ iama_checker/accounts/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import RegisterForm
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login
+from django.urls import reverse
 # Create your views here.
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
-            user.save()
-            return HttpResponseRedirect("/")
-    else:
-        form = RegisterForm
-
-    return render(request, "accounts/register.html", {"form": form})
+            # Check if user entered password is spelled correctly
+            if form.cleaned_data["password"] == form.cleaned_data["password_confirm"]:
+                user = User.objects.create_user(username=form.cleaned_data["username"], password=form.cleaned_data["password"])
+                user.save()
+                # Log the user in and send him to his home screen
+                login(request, user)
+                return HttpResponseRedirect(reverse("base:home"))
+        else:
+            error = "De ingevoerde wachtwoorden komen niet overeen!"
+    
+    form = RegisterForm()
+    return render(request, "accounts/register.html", {"form": form, "error": error})
     
