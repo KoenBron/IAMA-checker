985205541ccec2743fc2f9648c4e5bbf7135c53c
Koen Bron <koenbron2002@gmail.com>
2024-01-16 20:00:24 +0100

    Updated exception handling for register function

--- iama_checker/accounts/views.py
+++ iama_checker/accounts/views.py
@@ -9,16 +9,18 @@ def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
-            # Invalid input so jump back to register page 
-            if user is None:
-                error = "Invoer is incorrect!"
+            try:
+                user = User.objects.create_user(username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
+            # No username given 
+            except ValueError:
+                error = "Er moet een gebruikersnaam ingevoerd worden!"
 
             else:
                 user.save()
                 # Log the user in and send him to his home screen
                 login(request, user)
                 return HttpResponseRedirect(reverse("base:home"))
+
         # The is_valid() returns false if password1 and password2 don't match, src=https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
         else:
             error = "De ingevoerde wachtwoorden komen niet overeen!"
