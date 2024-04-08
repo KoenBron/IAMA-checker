7634ee983faf58b60a4a79174437ea2737783dab
Koen Bron <koenbron2002@gmail.com>
2024-04-08 19:36:08 +0200

    Fixed unexpected register bug

--- iama_checker/accounts/urls.py
+++ iama_checker/accounts/urls.py
@@ -8,5 +8,5 @@ app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("login/", auth_views.LoginView.as_view(authentication_form=forms.UserLoginForm))
+    path("login/", auth_views.LoginView.as_view(authentication_form=forms.UserLoginForm), name="login")
 ]
--- iama_checker/accounts/views.py
+++ iama_checker/accounts/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import login
 from django.urls import reverse
 # Create your views here.
 def register(request):
+    # Create placeholders  
+    context = {}
+    form = RegisterForm()
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -13,8 +17,9 @@ def register(request):
                 user = User.objects.create_user(username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
             # No username given 
             except ValueError:
-                error = "Er moet een gebruikersnaam ingevoerd worden!"
-
+                context["error"] = "Er moet een gebruikersnaam ingevoerd worden!"
+            
+            # User can safely be created, on checking for double user credentials yet!
             else:
                 user.save()
                 # Log the user in and send him to his home screen
@@ -23,8 +28,8 @@ def register(request):
 
         # The is_valid() returns false if password1 and password2 don't match, src=https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
         else:
-            error = "De ingevoerde wachtwoorden komen niet overeen!"
-    
-    form = RegisterForm()
-    return render(request, "accounts/register.html", {"form": form, "error": error})
+            context["error"] = "De ingevoerde wachtwoorden komen niet overeen!" 
+
+    context["form"] = form 
+    return render(request, "accounts/register.html", context)
     
